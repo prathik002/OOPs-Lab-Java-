a18d84c6116b87ee55aa2d3ad81f92c8b84fa92b
Prathik Vasant <prathikvasant123@gmail.com>
2026-03-01 09:43:58 +0530

    aded user input

--- Program 1/ComplexNo.java
+++ Program 1/ComplexNo.java
@@ -1,27 +1,40 @@
-class ComplexNo{
-double real,img;
-ComplexNo(double r,double i)
-{
-real=r;
-img=i;
+import java.util.Scanner;
+
+class ComplexNo {
+    double real, img;
+
+    ComplexNo(double r, double i) {
+        real = r;
+        img = i;
+    }
+
+    public static ComplexNo sum(ComplexNo c1, ComplexNo c2) {
+        ComplexNo temp = new ComplexNo(0, 0);
+        temp.real = c1.real + c2.real;
+        temp.img = c1.img + c2.img;
+        return temp;
+    }
+
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter real part of first complex number: ");
+        double r1 = sc.nextDouble();
+        System.out.print("Enter imaginary part of first complex number: ");
+        double i1 = sc.nextDouble();
+
+        System.out.print("Enter real part of second complex number: ");
+        double r2 = sc.nextDouble();
+        System.out.print("Enter imaginary part of second complex number: ");
+        double i2 = sc.nextDouble();
+
+        ComplexNo c1 = new ComplexNo(r1, i1);
+        ComplexNo c2 = new ComplexNo(r2, i2);
+
+        ComplexNo temp = sum(c1, c2);
+
+        System.out.println("Sum is: " + temp.real + " + " + temp.img + "i");
+
+        sc.close();
+    }
 }
-
-
-public static ComplexNo sum(ComplexNo c1,ComplexNo c2)
-{
-ComplexNo temp=new ComplexNo(0,0);
-temp.real=c1.real+c2.real;
-temp.img=c1.img+c2.img;
-return temp;
-}
-
-
-public static void main(String args[])
-{
-ComplexNo c1=new ComplexNo(5.5,4);
-ComplexNo c2=new ComplexNo(1.2,3.5);
-ComplexNo temp=sum(c1,c2);
-System.out.println("Sum is: " + temp.real + " + " + temp.img + "i");
-}
-}
-
